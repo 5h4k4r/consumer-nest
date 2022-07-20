f016d7307e5a263b07e90d0d7e240c5ec136f483
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-20 17:58:25 +0300

    Update app controller: Add a listener to the message

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @MessagePattern('rabbit-mq-producer')
+  public async execute(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    console.log(new Date().toLocaleTimeString(), 'data: ', data);
+    await this.appService.mySuperLongProcessOfUser(data);
+
+    channel.ack(originalMessage);
+  }
+}
