06adad9f3fbb83bd89542f4088dd13983761a1ed
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-20 17:57:56 +0300

    Update appservice

---
+++ src/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  mySuperLongProcessOfUser(data: any) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        console.log(`done processing ${JSON.stringify(data)}`);
+        resolve();
+      }, 10000);
+    });
+  }
+}
