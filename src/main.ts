a379babbac642575e3c50cafa1d0dbc045fa3937
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-29 16:35:54 +0300

    prettier

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
-        queue: 'demo-queue',
-      },
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
+      queue: 'demo-queue',
+      // false = manual acknowledgement; true = automatic acknowledgment
+      noAck: false,
+      // Get one by one
+      prefetchCount: 1,
+      durable: false,
     },
-  );
+  });
 
   await app.listen();
 }
