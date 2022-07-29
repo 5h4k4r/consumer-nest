71ae6ab4f5ad04e93edd24a7f158fb6290fc9274
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-29 16:11:17 +0300

    Update main ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
-      queue: 'demo-queue',
-      // false = manual acknowledgement; true = automatic acknowledgment
-      // noAck: false,
-      // Get one by one
-      // prefetchCount: 1,
-      durable: false,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
+        queue: 'demo-queue',
+      },
     },
-  });
+  );
 
   await app.listen();
 }
