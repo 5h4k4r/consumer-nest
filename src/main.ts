f85876801c21cdeca6fc496aba3c6569cb7cdfaf
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-29 15:58:21 +0300

    delay creation of the app

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,21 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
-      queue: 'demo-queue',
-      // false = manual acknowledgement; true = automatic acknowledgment
-      // noAck: false,
-      // Get one by one
-      // prefetchCount: 1,
-      durable: false,
-    },
-  });
+  setTimeout(() => {
+    const app = await NestFactory.createMicroservice(AppModule, {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
+        queue: 'demo-queue',
+        // false = manual acknowledgement; true = automatic acknowledgment
+        // noAck: false,
+        // Get one by one
+        // prefetchCount: 1,
+        durable: false,
+      },
+    });
 
-  await app.listen();
-}
-bootstrap();
+    await app.listen();
+
+  }, 3000);
+  bootstrap();
