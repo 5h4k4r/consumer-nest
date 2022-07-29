d11b1cede4fb42b8fec95adf484d212202eb6dbf
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-29 12:04:00 +0300

    Update rmq connection string

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://john123:123456@localhost:15672/demo-vhost'],
+      urls: ['amqp://john123:123456@localhost:5672/demo-vhost'],
       queue: 'demo-queue',
       // false = manual acknowledgement; true = automatic acknowledgment
       noAck: false,
